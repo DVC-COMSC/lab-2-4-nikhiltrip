35753b9b5e2d3b6ed0e6b83a5c7dcad74dadc96b
Nikhil Tripathi <nikhiltrip1@gmail.com>
2023-06-24 20:51:41 -0700

    create main.py

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ def main():
     # Comlete your code here
     ##################################################
     pass
+    original_str="Python Programming"
+    sub1=original_str[0:6]
+    sub2=original_str[7:len(original_str)]
+    merged_str=sub1+sub2
+    print(sub2)
+    print(sub1)
+    print(merged_str)
 
 
 if __name__ == '__main__':
